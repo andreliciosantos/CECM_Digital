7bb1c8bf6e1dd5c5f38ae9169eb8eee08ba271c5
Andrelicio Santos <andreliciosantos@gmail.com>
2026-01-02 13:28:30 -0300

    define schema.prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { StudentsModule } from './students/students.module';
+import { ClassesModule } from './classes/classes.module';
+import { ProfessorsModule } from './professors/professors.module';
+import { ManagementModule } from './management/management.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, StudentsModule, ClassesModule, ProfessorsModule, ManagementModule],
   controllers: [AppController],
   providers: [AppService],
 })
